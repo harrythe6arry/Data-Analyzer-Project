e2c79b9c508f92e3cd2b52e4ccad2351f6da2ac7
harrythe6arry <voravit.sri@student.mahidol.edu>
2024-07-04 09:48:53 +0700

    Added plotting functions, csv generator using python, and example, First version without making anything parallel yet

--- README.md
+++
@@ -1,2 +0,0 @@
-# FunPar-Project
-This is my project for data analyzer. Hello my name is harry and this is my first commit checking. 
--- data-analyzer-ta/src/indicator/mod.rs
+++
@@ -1,5 +0,0 @@
-// src/indicator/mod.rs
-pub mod exponentialMovingAverage;
-
-// Re-export the ExponentialMovingAverage so it can be accessed directly from `indicator`
-pub use exponentialMovingAverage::ExponentialMovingAverage;
